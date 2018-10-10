783f11a7c3f250bab296f8e05acb7a55ae4aceaa
AndrewDitty <AndrewDitty@gmail.com>
2018-10-10 11:48:35 -0400

    Implemented methods validateUserInput and selectOption, as well as tests for validateUserInput

--- src/paystation/domain/DisplayImpl.java
+++ src/paystation/domain/DisplayImpl.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package paystation.domain;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -19,7 +18,8 @@ public class DisplayImpl implements Display {
     // A main() program should be developed to simulate the PayStation operation. 
 
     //It puts up a menu to allow a customer to select a choice:
-            
+    
+    private final int MAX_INVALID=10; 
     private final String MENU = "    1. Deposit Coins\n" +
             "    2. Display\n" +
             "    3. Buy Ticket\n" +
@@ -49,14 +49,51 @@ public class DisplayImpl implements Display {
     }
     
     @Override 
-    public int validateUserInput(){
-        int choice = 0;
-        Scanner scan = new Scanner(System.in);
-
-        do{
-            System.out.print(MENU);
-            choice = scan.nextInt();
-        }while(choice<1 || choice>5);
+    public int validateUserInput(String input){
+        int i = -1;
+        try{
+            i = Integer.parseInt(input);
+        }catch(Exception e){
+            System.out.print("Invalid selection. Integers only. Enter 1-5 only\n");
+            return -1;
+        }
+        
+        if (i>=1 && i<=5){
+            return i;
+        }else{
+            System.out.print("Invalid selection. Enter 1-5 only\n");
+            i = -1;
+            
+        }
+        return i;
+    }
+        
+    @Override
+    public int selectOption(){
+        int choice;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please select an option numerically (1-5)\n");
+        String raw = null;
+        
+        for(int loops=0; loops<MAX_INVALID; loops++){
+            try{
+                raw = sc.nextLine();
+            }catch(Exception e){
+                System.out.print("Invalid input/n");
+            }finally{
+                choice = validateUserInput(raw);
+            }
+            
+            if(choice != -1){
+                return choice;
+            }
+        }
+        
+        //if this code is reached then user entered too many invalid inputs
+        System.out.print(MAX_INVALID + " invalid inputs have been enter successively/n"+
+                "This transaction will now cancel");
+        return 4;
     }
     
 }
