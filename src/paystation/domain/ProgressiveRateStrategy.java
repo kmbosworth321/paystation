c09d176f0241a42d3d731471640d0d266e2ffad0
Shmuel Jacobs <tuf65651@temple.edu>
2018-10-12 14:29:05 -0400

    Implemented Progressive Strategy

--- src/paystation/domain/ProgressiveRateStrategy.java
+++ src/paystation/domain/ProgressiveRateStrategy.java
@@ -4,6 +4,17 @@ public class ProgressiveRateStrategy implements RateStrategy {
 
     @Override
     public int calculatePayment(int inserted) {
-        return 0;
+
+        if(inserted <= 150) {
+            return 2 * inserted / 5;
+        } else if (inserted <= 350) {
+            int time = 60;
+            time += 1.5 * (inserted - 150) / 5;
+            return time;
+        } else {
+            int time = 120;
+            time += (inserted - 350) / 5;
+            return time;
+        }
     }
 }
