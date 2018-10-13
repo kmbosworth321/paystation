b72a11f8610574812e946b81f7ef3c93cef57c85
AndrewDitty <AndrewDitty@gmail.com>
2018-10-13 19:59:39 -0400

    implemented alternatingRateStrategy

--- src/paystation/domain/AlternatingRateStrategy.java
+++ src/paystation/domain/AlternatingRateStrategy.java
@@ -1,5 +1,8 @@
 package paystation.domain;
 
+import java.util.Calendar;
+import java.util.HashMap;
+
 public class AlternatingRateStrategy implements RateStrategy {
     private RateStrategy weekdayStrategy;
     private RateStrategy weekendStrategy;
@@ -11,7 +14,30 @@ public class AlternatingRateStrategy implements RateStrategy {
 
 
     @Override
-    public int calculatePayment() {
-        return 0;
+    public int calculatePayment(int inserted) {
+        //find out what day it is
+        
+        int strategy;
+        //Get day
+        int day = Calendar.DAY_OF_WEEK;
+ 
+        //check what strategy is employed on that day
+        int defaultRate = 0;//this is returned in case of error
+        HashMap<Integer, Integer> rateMap= new HashMap<>();
+        rateMap.put(1, 0);//Monday's rateStrategy
+        rateMap.put(2, 0);//Tuesday
+        rateMap.put(3, 0);//Wednesday
+        rateMap.put(4, 0);//Thursday
+        rateMap.put(5, 0);//Friday
+        rateMap.put(6, 1);//Saturday
+        rateMap.put(7, 1);//Sunday
+        
+        strategy = rateMap.get(day);
+        
+        if (strategy == 1){//special rate (progressive)
+            return weekendStrategy.calculatePayment(inserted);
+        }else{//default rate (linear)
+            return weekendStrategy.calculatePayment(inserted);
+        }//Note: add more strategies by adding more options
     }
 }
