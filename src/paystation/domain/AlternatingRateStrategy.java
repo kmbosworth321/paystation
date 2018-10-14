b9f0b1430b7b13688b2ba2da680cbfe85be5a7ed
AndrewDitty <AndrewDitty@gmail.com>
2018-10-13 21:37:15 -0400

    cleaned up alternatingRateStrategy

--- src/paystation/domain/AlternatingRateStrategy.java
+++ src/paystation/domain/AlternatingRateStrategy.java
@@ -2,28 +2,18 @@ package paystation.domain;
 
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
+
 
 public class AlternatingRateStrategy implements RateStrategy {
     private RateStrategy weekdayStrategy;
     private RateStrategy weekendStrategy;
+    private HashMap<Integer, Integer> rateMap;
 
-    public AlternatingRateStrategy(RateStrategy weekdayStrategy, RateStrategy weekendStrategy) {
-        this.weekdayStrategy = weekdayStrategy;
-        this.weekendStrategy = weekendStrategy;
-    }
-
-
-    @Override
-    public int calculatePayment(int inserted) {
-        //find out what day it is
-        
-        int strategy;
-        //Get day
-        int day = Calendar.DAY_OF_WEEK;
- 
-        //check what strategy is employed on that day
-        int defaultRate = 0;//this is returned in case of error
-        HashMap<Integer, Integer> rateMap= new HashMap<>();
+    public AlternatingRateStrategy() {
+        this.weekdayStrategy = new LinearRateStrategy();
+        this.weekendStrategy  = new ProgressiveRateStrategy();
+        this.rateMap= new HashMap<>();
         rateMap.put(1, 0);//Monday's rateStrategy
         rateMap.put(2, 0);//Tuesday
         rateMap.put(3, 0);//Wednesday
@@ -31,13 +21,18 @@ public class AlternatingRateStrategy implements RateStrategy {
         rateMap.put(5, 0);//Friday
         rateMap.put(6, 1);//Saturday
         rateMap.put(7, 1);//Sunday
+    }
+
+    @Override
+    public int calculatePayment(int inserted) {
+        int strategy;
+        int day = Calendar.DAY_OF_WEEK;
         
         strategy = rateMap.get(day);
-        
-        if (strategy == 1){//special rate (progressive)
+        if (strategy == 1){
             return weekendStrategy.calculatePayment(inserted);
-        }else{//default rate (linear)
-            return weekendStrategy.calculatePayment(inserted);
+        }else{
+            return weekdayStrategy.calculatePayment(inserted);
         }//Note: add more strategies by adding more options
     }
 }
